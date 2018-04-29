61cc0955c2ccee2ec45117d835c2b2acb6d4d268
Winreer324 <32igor3@mail.ru>
2018-04-29 19:05:05 +0300

    test

--- pr 9/js.js
+++ pr 9/js.js
@@ -1,10 +1,11 @@
 function get_name_browser(){ 
-    let agent = navigator.userAgent;     
-    if (agent.search(/Chrome/) > 1) return 'Google Chrome';
-    if (agent.search(/Firefox/) || agent.search(/Gecko/) > 1) return 'Firefox';
-    if (agent.search(/Opera/) > 1) return 'Opera';
-    if (agent.search(/Safari/) > 1) return 'Safari';
-    if (agent.search(/MSIE/) > 1) return 'Internet Explorer';
+    let agent = navigator.userAgent;  
+    if (agent.search(/OPR/) > 1) return "Opera";    
+    if (agent.search(/Firefox/) > 1) return "Firefox"; 
+    if (agent.search(/Trident/) > 1) return "Internet Explorer";
+    if (agent.search(/Edge/) > 100) return "Microsoft Edge";
+    if (agent.search(/Chrome/) > 80) return "Google Chrome"; 
+    if (agent.search(/Safari/) > 1) return "Safari";
     return 'Не определен';
 }
 
