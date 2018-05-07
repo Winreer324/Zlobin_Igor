30e02aec2a5488562dee3883a9711096a825a326
Winreer324 <32igor3@mail.ru>
2018-05-07 21:42:40 +0300

    test

--- pr 6/js.js
+++ pr 6/js.js
@@ -2,11 +2,10 @@
 let stud = {
 	name: "Ivanov",
 	old: 21,
-	sayName:()=>{
-		console.log("Name is " + stud.name);
+	sayName:function(){
+		alert("Name is " + stud.name);
 	}
-}
-
+} 
 let book = {
 	title:"BDV",
 	pages:654,
@@ -14,37 +13,61 @@ let book = {
 	avtor:"Hign"
 }
 let name = {
-	name:"Igor last"
-}
+	name:"Igor last",
+	name1:"Igor123 last",
+	check2:function(){  alert("hello2"); },
+	check1:function(){  alert("hello1"); }
+}	
 let car = {
 	speed:210
-}
-let arr = [stud,book,name,car]; 
+} 
 let temp ={};
-function callback(){
-	console.log("последний объект :",arr[arr.length - 1]);  
-	temp = arr[arr.length - 1];
-	for(var key in temp){
-		console.log(temp[key]);
-	}
-}; 
-let a = arr[arr.length - 1];
-let test = (arr,callback)=>{
-	let n = 0; 
+let last = {
+	check:function(){  alert("hello"); },
+	check1:function(){  alert("hello1"); },
+	check2:function(){  alert("hello2"); }
+}
+let arr = [stud,book,car,last,name]; 
+let a = arr[arr.length - 1]; 
+let test = (arr,callback)=>{ 
 	let max = 0;
-	for (var i = 0; i < arr.length; i++) {
-		let x = 0;
+	let index = 0; // для возврата max object
+	
+	for (let i = 0; i < arr.length; i++) {
+		let x = 0;// счет полей   
 		for (let a in arr[i]) { // считаем кол-во методов в arr[i]
 			x++;
-		} 
-		if(max < x){ max = x;n=i;} // max 
-	}   
-	if(callback){
-	callback(); 
-	}
-	else console.log("нет callback");
-
+			if(max < x){ max = x; index = i;} // max  
+		}  
+	}    
+	////////// проверка на function
+	let check_F = true; 
+	let last_i = 0;
+ 	for (let key in a){ 
+ 		if(typeof a[key] === "function") check_F = true;
+ 		else check_F = false; 
+ 		last_i++;
+ 	}   
+ 	callback();
 	console.log("макс методов в этом объекте :"); 
-	return arr[n]; 
+	return arr[index]; 
 }
-//  test(arr,callback)
+test(arr,function(){ 
+	let last_i = 0;
+ 	for (let key in a){  
+ 		last_i++;
+ 	}   
+	for(let i = last_i;i > 0; i--){
+		let q = true;
+		for (let key in a){  
+ 			if(typeof a[key] === "function")
+	 	    {
+	 			temp = a[key];  
+	 			temp();
+				q = false; 
+	 		}  
+	 		if(!q) break;
+ 		} 
+ 		if(!q) break;
+	}  
+});
